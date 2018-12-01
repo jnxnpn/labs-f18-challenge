2ff697c383639c30240418ecbd0b93d24f4a51f7
Pannnn <7545015+jnxnpn@users.noreply.github.com>
2018-12-01 13:58:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
-
+@app.route('/pokemon/<query>', methods=['GET'])
+def main(query):
+	r = requests.get('https://pokeapi.co/api/v2/pokemon/'+query+'/')
+	js = r.json()
+	##query = query +''
+	if query.isdigit():
+		st = "the pokemon with number "+ query+" has name " + js["name"]
+	else:
+		st = "the pokemon with name "+query + " has number " + str(js["id"])
+	return st
 
 if __name__ == '__main__':
     app.run()
